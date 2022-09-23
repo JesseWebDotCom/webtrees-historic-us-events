206af072f592dffb8c0c48b70c73b3c3af02fc53
Jesse Torres <20848952+JesseWebDotCom@users.noreply.github.com>
2022-09-22 21:57:29 -0400

    removed language check

--- module.php
+++ module.php
@@ -161,30 +161,22 @@ return new class extends AbstractModule implements ModuleCustomInterface, Module
     
     public function historicEventsAll(): Collection
     {
-        switch (I18N::languageTag()) {
-            case 'en-AU':
-            case 'en-GB':
-            case 'en-US':
-                return new Collection([
-                    "1 EVEN Worldwide economic depression \n2 TYPE Great Depression\n2 DATE FROM AUG 1929 TO 1939\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Great_Depression)",
-                    "1 EVEN War between Axis Powers (Germany, Italy, Japan) and the Allied Powers (Britain, United States, Soviet Union, France) \n2 TYPE World War II\n2 DATE FROM 1 SEP 1939 TO 2 SEP 1945\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/World_War_II)",
-                    "1 EVEN Imperial Japanese Navy Air attack the US Pearl Harbor naval base \n2 TYPE Attack on Pearl Harbor\n2 DATE 7 DEC 1941\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Attack_on_Pearl_Harbor)",
-                    "1 EVEN War between North Korea and South Korea \n2 TYPE Korean War\n2 DATE FROM 25 JUN 1950 TO 27 JUL 1953\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Korean_War)",
-                    "1 EVEN War between North Vietnam against South Vietnam \n2 TYPE Vietnam War\n2 DATE FROM 1 NOV 1955 TO 30 APR 1975\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Korean_War)",
-                    "1 EVEN Assassination of United States president John F. Kennedy \n2 TYPE John F. Kennedy Assassination \n2 DATE 22 NOV 1963\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Assassination_of_John_F._Kennedy)",
-                    "1 EVEN Assassination of Baptist minister and civil rights leader Martin Luther King Jr. \n2 TYPE Martin Luther King, Jr. Assassination \n2 DATE 4 APR 1968\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Assassination_of_Martin_Luther_King_Jr.)",
-                    "1 EVEN Apollo 11 Moon landing\n2 TYPE Moon landing\n2 DATE 20 JUL 1969\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Moon_landing)",
-                    "1 EVEN Explosion of the U.S. space shuttle orbiter Challenger, shortly after its launch \n2 TYPE Space Shuttle Challenger disaster\n2 DATE 28 JAN 1986\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Space_Shuttle_Challenger_disaster)",
-                    "1 EVEN International conflict triggered by Iraq's invasion of Kuwait \n2 TYPE Gulf War\n2 DATE FROM 2 AUG 1990 TO 28 FEB 1991\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Gulf_War)",
-                    "1 EVEN Projected but mostly unrealized havoc on computerized systems during the transition from the year 1999 to 2000 \n2 TYPE Y2K \n2 DATE 1 JAN 2000\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Year_2000_problem)",
-                    "1 EVEN Terrorists attack World Trade Center and Pentagon \n2 TYPE September 11 attacks (9/11)\n2 DATE 11 SEP 2001\n2 NOTE [wikipedia](https://wikipedia.org/wiki/September_11_attacks)",
-                    "1 EVEN Ongoing global pandemic of coronavirus disease 2019 (COVID-19) \n2 TYPE COVID-19 pandemic\n2 DATE FROM 17 NOV 2019\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/COVID-19_pandemic)",
-                ]);
-
-    default:
-        return new Collection();
-}
-}
+        return new Collection([
+            "1 EVEN Worldwide economic depression \n2 TYPE Great Depression\n2 DATE FROM AUG 1929 TO 1939\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Great_Depression)",
+            "1 EVEN War between Axis Powers (Germany, Italy, Japan) and the Allied Powers (Britain, United States, Soviet Union, France) \n2 TYPE World War II\n2 DATE FROM 1 SEP 1939 TO 2 SEP 1945\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/World_War_II)",
+            "1 EVEN Imperial Japanese Navy Air attack the US Pearl Harbor naval base \n2 TYPE Attack on Pearl Harbor\n2 DATE 7 DEC 1941\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Attack_on_Pearl_Harbor)",
+            "1 EVEN War between North Korea and South Korea \n2 TYPE Korean War\n2 DATE FROM 25 JUN 1950 TO 27 JUL 1953\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Korean_War)",
+            "1 EVEN War between North Vietnam against South Vietnam \n2 TYPE Vietnam War\n2 DATE FROM 1 NOV 1955 TO 30 APR 1975\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Korean_War)",
+            "1 EVEN Assassination of United States president John F. Kennedy \n2 TYPE John F. Kennedy Assassination \n2 DATE 22 NOV 1963\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Assassination_of_John_F._Kennedy)",
+            "1 EVEN Assassination of Baptist minister and civil rights leader Martin Luther King Jr. \n2 TYPE Martin Luther King, Jr. Assassination \n2 DATE 4 APR 1968\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Assassination_of_Martin_Luther_King_Jr.)",
+            "1 EVEN Apollo 11 Moon landing\n2 TYPE Moon landing\n2 DATE 20 JUL 1969\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Moon_landing)",
+            "1 EVEN Explosion of the U.S. space shuttle orbiter Challenger, shortly after its launch \n2 TYPE Space Shuttle Challenger disaster\n2 DATE 28 JAN 1986\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Space_Shuttle_Challenger_disaster)",
+            "1 EVEN International conflict triggered by Iraq's invasion of Kuwait \n2 TYPE Gulf War\n2 DATE FROM 2 AUG 1990 TO 28 FEB 1991\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Gulf_War)",
+            "1 EVEN Projected but mostly unrealized havoc on computerized systems during the transition from the year 1999 to 2000 \n2 TYPE Y2K \n2 DATE 1 JAN 2000\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/Year_2000_problem)",
+            "1 EVEN Terrorists attack World Trade Center and Pentagon \n2 TYPE September 11 attacks (9/11)\n2 DATE 11 SEP 2001\n2 NOTE [wikipedia](https://wikipedia.org/wiki/September_11_attacks)",
+            "1 EVEN Ongoing global pandemic of coronavirus disease 2019 (COVID-19) \n2 TYPE COVID-19 pandemic\n2 DATE FROM 17 NOV 2019\n2 NOTE [wikipedia](https://en.wikipedia.org/wiki/COVID-19_pandemic)",
+        ]);
+    }
     
 };
  
